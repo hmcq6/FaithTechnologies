ff5f1559ff2cd1a10d7efe0aa3b6e4775c04517c
Hassan McKusick <Hassan.McKusick@gmail.com>
2012-10-18 19:52:37 -0400

    Initial Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaithTechnologies::Application.config.secret_token = 'caae7cf2c24a1bf7d787afa16f56c1c0bb27ceb211a5e53ecbc24c0589a690ef43aadebd66d3e92c2712a179e382ad6fbca6e1df061204340949a5acbe6c9911'
